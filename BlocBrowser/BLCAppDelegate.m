25e74364b4526edd15b7a472d8a462b2fcc62098
Jeanne Sneed <jsneed@gmail.com>
2014-07-19 21:51:46 -0700

    Create BlocBrowser

--- BlocBrowser/BLCAppDelegate.m
+++ BlocBrowser/BLCAppDelegate.m
@@ -7,14 +7,18 @@
 //
 
 #import "BLCAppDelegate.h"
+#import "BLCWebBrowserViewController.h"
 
 @implementation BLCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BLCWebBrowserViewController alloc] init]];
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Jeanne on 7/19/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
